4c76413c97a3064a3c49b3399e9da3c697fa1f7b
Niraj Singh <singhniraj@google.com>
2024-05-07 12:12:09 +0530

    message

---
+++ app/helper_function.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    """Adds two numbers"""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers"""
+    return x - y
+
+def multiply(x, y):
+    """Multiplies two numbers"""
+    return x * y
+
+def divide(x, y):
+    """Divides two numbers"""
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
